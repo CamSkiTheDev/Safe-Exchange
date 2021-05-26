493a54489a41609ef0524d615e613297f7cbcc92
Tani Robinson <tanisha.robinson94@aol.com>
2021-05-26 18:35:44 -0400

    Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,17 +7,25 @@ function Header(props) {
   return (
       <div >
         <nav className="navbar is-primary">
-            <Link to="/" className="navbar-item">
-                <img src="https://storage.googleapis.com/dpss-wordpress-prod/2/2017/01/Safe-Exchange-Zone-Logo.png"></img>
-            </Link>
-            <Link to="/">
-                <div>View all logs</div>
-            </Link>
-        {currentUser ? (
-            <Link onClick={logout}>Logout</Link>
-        ) : (
-            <Link to="login">Login</Link>
-        )}
+            <div className="navbar-item">
+                <Link to="/" className="navbar-item">
+                    <img src="https://storage.googleapis.com/dpss-wordpress-prod/2/2017/01/Safe-Exchange-Zone-Logo.png" className="logo" width="112" height="40"></img>
+                </Link>
+            </div>
+            <div className="navbar-end">
+                <div className="navbar-item button is-warning">
+                    <Link to="/logs">
+                        <div>View all logs</div>
+                    </Link>
+                </div>
+                <div className="navbar-item button is-danger">
+                {currentUser ? (
+                    <Link onClick={logout}>Logout</Link>
+                ) : (
+                    <Link to="login">Login</Link>
+                )}
+                </div>
+            </div>
         </nav>
     </div>
   );
