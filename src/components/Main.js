b605f80a130dfd2cc8aa875266cffa920df59d62
Tani Robinson <tanisha.robinson94@aol.com>
2021-05-26 20:32:22 -0400

    badding variables in Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import Logs from "../pages/Logs";
 import Show from "../pages/Show";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import LandingPage from "../pages/LandingPage";
 
 function Main(props) {
+  const [logs, setLogs] = useState(null);
+
+  const URL = " ";
+
+  const getLogs = async () => {
+
+  };
+
+  const createLog = async () => {
+
+  }
+
+  const deleteLogs = async (id) => {
+
+  }
+
+  useEffect(() => getLogs(), []);
+
   return (
     <main>
       <Switch>
+        <Route exact path="/" component={LandingPage} />
         <PrivateRoute exact path="/logs" component={Logs} />
         <PrivateRoute path="/logs/:id" component={Show} />
         <Route path="/login" render={(rp) => <Login {...rp} />} />
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -2,4 +2,4 @@ function Show(props) {
     return <h1>Show</h1>
 }
 
-export default Show
+export default Show   
